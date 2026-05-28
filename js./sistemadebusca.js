95a7e5629a2166d91ed1304915c717c01e2c4eca
felpiz-1 <rochadeandradef@gmail.com>
2026-05-27 22:02:04 -0300

    criação de arrey

--- js./sistemadebusca.js
+++ js./sistemadebusca.js
@@ -1,3 +1,36 @@
+let produto = [
+    {id:1,
+     nome:"Bomba de Pistão",
+     img:"img/bomba1.webp",
+     legenda:"Caso tenha alguma dúvida sobre esse item, por gentileza entrar em contato conosco através do whatsapp",
+     valor:2500.00
+    },
+    {id:1,
+     nome:"Bomba de Engrenagem",
+     img:"img/bomba2.webp",
+     legenda:"Caso tenha alguma dúvida sobre esse item, por gentileza entrar em contato conosco através do whatsapp",
+     valor:2400.00
+    },
+    {id:1,
+     nome:"Bomba DYNAPAC",
+     img:"img/bomba3.webp",
+     legenda:"Caso tenha alguma dúvida sobre esse item, por gentileza entrar em contato conosco através do whatsapp",
+     valor:3100.00
+    },
+    {id:1,
+     nome:"Bomba ESCAVADEIRA",
+     img:"img/bomba4.webp",
+     legenda:"Caso tenha alguma dúvida sobre esse item, por gentileza entrar em contato conosco através do whatsapp",
+     valor:4500.00
+    },
+    {id:1,
+     nome:"Bomba A15VSO",
+     img:"img/bomba5.webp",
+     legenda:"Caso tenha alguma dúvida sobre esse item, por gentileza entrar em contato conosco através do whatsapp",
+     valor:2900.00
+    },
+]
+
 const btn = document.getElementById("btnbusca")
 
 btn.addEventListener('click', (event) =>{
